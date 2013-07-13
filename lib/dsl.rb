d28d6488b2daff046b6bab2d415f44e7f0dcb10b
James Childress <james.childress@me.com>
2013-07-13 15:41:47 -0700

    added comments to DSL ClassMethods and InstanceMethods.

--- lib/dsl.rb
+++ lib/dsl.rb
@@ -4,36 +4,49 @@ module XSemVer
     
     def self.included(klass)
       klass.extend ClassMethods
+      klass.send :include, InstanceMethods
     end
 
-
-
-
-    COMMAND_PREFIX = :_run_
-    
     class CommandError < StandardError
     end    
     
-    def run_command(command)
-      method_name = "#{COMMAND_PREFIX}#{command}"
-      if self.class.method_defined?(method_name)
-        send method_name
-      else
-        raise CommandError, "invalid command #{command}"
+    
+    
+    
+    module InstanceMethods
+      
+      # Calls an instance method defined via the ::command class method.
+      # Raises CommandError if the command does not exist.
+      def run_command(command)
+        method_name = "#{self.class.command_prefix}#{command}"
+        if self.class.method_defined?(method_name)
+          send method_name
+        else
+          raise CommandError, "invalid command #{command}"
+        end
       end
-    end    
+      
+    end
 
 
 
 
     module ClassMethods
       
+      # Defines an instance method based on the first command name.
+      # The method executes the code of the given block.
+      # Aliases methods for any subsequent command names.
       def command(*command_names, &block)
-        method_name = "#{COMMAND_PREFIX}#{command_names.shift}"
+        method_name = "#{command_prefix}#{command_names.shift}"
         define_method method_name, &block
         command_names.each do |c|
-          class_eval "alias :#{COMMAND_PREFIX}#{c} :#{method_name}"
+          class_eval "alias :#{command_prefix}#{c} :#{method_name}"
         end
+      end
+      
+      # The prefix for any instance method defined by the ::command method.
+      def command_prefix
+        :_run_
       end
       
     end
